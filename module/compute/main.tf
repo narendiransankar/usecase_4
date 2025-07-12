214e6814e9cc6a60ba76c03774f814e146a6321d
narendiransankar <narendiransankar1699@gmail.com>
2025-07-12 15:12:44 +0530

    Update main.tf

--- module/compute/main.tf
+++ module/compute/main.tf
@@ -1,1 +1,16 @@
 
+
+resource "aws_instance" "web" {
+  count         = length(var.public_subnet_ids)
+  ami           = "ami-020cba7c55df1f615"
+  instance_type = var.instance_type
+  subnet_id     = var.public_subnet_ids[count.index]
+  associate_public_ip_address = true
+  vpc_security_group_ids = [var.web_sg_id]
+
+  user_data = file("${path.module}/userdata.sh")
+
+  tags = {
+    Name = "${var.env}-web-${count.index}"
+  }
+}

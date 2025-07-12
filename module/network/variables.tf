431ffcc90e70c19e3584f67d9676a50d886cd46c
narendiransankar <narendiransankar1699@gmail.com>
2025-07-12 15:17:38 +0530

    Create variables.tf

---
+++ module/network/variables.tf
@@ -0,0 +1,24 @@
+variable "env" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}

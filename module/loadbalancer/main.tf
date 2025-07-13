4659e9bd7ebf210928159cb7c8b6bc8b3b27ff7b
narendiransankar <narendiransankar1699@gmail.com>
2025-07-13 11:10:39 +0530

    Update main.tf

--- module/loadbalancer/main.tf
+++ module/loadbalancer/main.tf
@@ -58,6 +58,13 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0

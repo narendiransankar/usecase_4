b775220f4ac4f84b879a324d3fdb7d8199b2fc66
narendiransankar <narendiransankar1699@gmail.com>
2025-07-13 11:13:06 +0530

    Update main.tf

--- module/loadbalancer/main.tf
+++ module/loadbalancer/main.tf
@@ -58,13 +58,15 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
